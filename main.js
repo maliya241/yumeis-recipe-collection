8436967ea6c9eb00ee49e28694e7349fcb7f7945
Maria McConkey <maliya241@users.noreply.github.com>
2020-12-20 17:12:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-
 var multiplier = Number(document.getElementById("serving_size_multiplier").value);
 var number_of_ingredients = document.getElementsByClassName("measurement").length;
 var fraction_ingredient_measurements = [];
 var decimal_ingredient_measurements = [];
-var original_ingredient_measurement_units = [];
-var modified_ingredient_measurement_units = [];
+var ingredient_measurement_units = [];
 
 //read in the ingredient measurements
 for (i = 0; i < number_of_ingredients; i++) {
@@ -12,17 +10,17 @@ for (i = 0; i < number_of_ingredients; i++) {
 	fraction_ingredient_measurements[i] = document.getElementsByClassName("measurement")[i].innerHTML; 
 	
 	//read in measure unit string
-	original_ingredient_measurement_units[i] = document.getElementsByClassName("measure_unit")[i].innerHTML; 
+	ingredient_measurement_units[i] = document.getElementsByClassName("measure_unit")[i].innerHTML; 
 	
-	//Nested array. For six element nested arrays, 0 == whole number, 1 == " ", 2 == numerator, 3 == "/", 4 == first denominator, 5 == second digit denominator; For five element nested arrays, 0 == whole number, 1 == " ", 2 == numerator, 3 == "/", 4 == denominator; For three element nested arrays, 0 == numerator, 1 == "/" 2 == denominator or all are whole numbers
+	//Nested array. For six element nested arrays, 0 == whole number, 1 == " ", 2 == numerator, 3 == "/", 4 == first denominator, 5 == second digit denominator; For five element nested arrays, 0 == whole number, 1 == " ", 2 == numerator, 3 == "/", 4 == denominator; For three element nested arrays, 0 == numerator, 1 == "/" 2 == denominator
 	fraction_ingredient_measurements[i] = fraction_ingredient_measurements[i].split(""); 
 	
 	//Convert string fractions to decimal numbers
 	if (fraction_ingredient_measurements[i].length == 6 && fraction_ingredient_measurements[i][3] == "/") {
 		decimal_ingredient_measurements[i] = Number(fraction_ingredient_measurements[i][0]) + Number(fraction_ingredient_measurements[i][2]) / Number(fraction_ingredient_measurements[i][4]+fraction_ingredient_measurements[i][5]);
-	} else if (fraction_ingredient_measurements[i].length == 5 && fraction_ingredient_measurements[i][3] == "/") {
+	} else if (fraction_ingredient_measurements[i].length == 5  && fraction_ingredient_measurements[i][3] == "/") {
 		decimal_ingredient_measurements[i] = Number(fraction_ingredient_measurements[i][0]) + Number(fraction_ingredient_measurements[i][2]) / Number(fraction_ingredient_measurements[i][4]);
-	} else if (fraction_ingredient_measurements[i].length == 3 && fraction_ingredient_measurements[i][1] == "/") {
+	} else if (fraction_ingredient_measurements[i].length == 3  && fraction_ingredient_measurements[i][1] == "/") {
 		decimal_ingredient_measurements[i] = Number(fraction_ingredient_measurements[i][0]) / Number(fraction_ingredient_measurements[i][2]);
 	} else {
 		decimal_ingredient_measurements[i] = Number(fraction_ingredient_measurements[i][0]);
@@ -51,31 +49,12 @@ function after_multiplier_change() {
 	var multiplied_fraction_ingredients = [];
 	var whole_number_part_multiplied_decimal_ingredients = [];
 	var decimal_part_multiplied_decimal_ingredients = [];
-		
+	
 	for (i = 0; i < number_of_ingredients; i++) { 
 		//decimal ingredient measurements multiplied by the multiplier and stored in multiplied_decimal_ingredients array
 		multiplied_decimal_ingredients[i] = decimal_ingredient_measurements[i] * multiplier;
 		
-		//converts teaspoons to tablespoons and tablespoons to cups when appropriate
-		if (original_ingredient_measurement_units[i].match(/teaspoon$/g) == "teaspoon" || original_ingredient_measurement_units[i].match(/teaspoons$/g) == "teaspoons") {
-			var tsp_to_tbsp_decimal_value = Number("0." + String(multiplied_decimal_ingredients[i]/3).split(".")[1]);
-			if (tsp_to_tbsp_decimal_value == 1 || tsp_to_tbsp_decimal_value == 0.25 || tsp_to_tbsp_decimal_value == 0.5 || tsp_to_tbsp_decimal_value == 0.75) {
-				//converts teaspoons to tablespoons
-				multiplied_decimal_ingredients[i] = multiplied_decimal_ingredients[i]/3;
-				modified_ingredient_measurement_units[i] = "tablespoon";
-			}
-		} else if (original_ingredient_measurement_units[i].match(/tablespoon$/g) == "tablespoon" ||original_ingredient_measurement_units[i].match(/tablespoons$/g) == "tablespoons") {
-			var tbsp_to_cup_decimal_value = Number("0." + String(multiplied_decimal_ingredients[i]/16).split(".")[1]);
-			if (tbsp_to_cup_decimal_value == 1 || tbsp_to_cup_decimal_value == 0.125 || tbsp_to_cup_decimal_value == 0.25 || tbsp_to_cup_decimal_value == 0.375 || tbsp_to_cup_decimal_value == 0.5 || tbsp_to_cup_decimal_value == 0.625 || tbsp_to_cup_decimal_value == 0.75 || tbsp_to_cup_decimal_value == 0.875 || tbsp_to_cup_decimal_value == (1/3) || tbsp_to_cup_decimal_value == (2/3)) {
-				//converts tablespoons to cups
-				multiplied_decimal_ingredients[i] = multiplied_decimal_ingredients[i]/16;
-				modified_ingredient_measurement_units[i] = "cup";
-			}
-		} else {
-			modified_ingredient_measurement_units[i] = original_ingredient_measurement_units[i];
-		}
-		
-		//converts decimal to fraction
+		//convert decimals to fractions
 		whole_number_part_multiplied_decimal_ingredients[i] = Number(String(multiplied_decimal_ingredients[i]).split(".")[0]); 
 		decimal_part_multiplied_decimal_ingredients[i] = String(multiplied_decimal_ingredients[i]).split(".")[1];
 		var one_third = String(1/3).split(".")[1];
@@ -144,24 +123,22 @@ function after_multiplier_change() {
 		document.getElementsByClassName("measurement")[i].innerHTML = multiplied_fraction_ingredients[i];
 		
 		//changes singular to plural and vice versa
-		if (Number(multiplied_decimal_ingredients[i]) > Number(1) && modified_ingredient_measurement_units[i].match(/s$/g) != "s") {
+		if (Number(multiplied_decimal_ingredients[i]) > Number(1) && ingredient_measurement_units[i].match(/s$/g) != "s") {
 			//singular to plural
-			if (modified_ingredient_measurement_units[i].endsWith("x")) {
-				modified_ingredient_measurement_units[i] = modified_ingredient_measurement_units[i] + "es";
+			if (ingredient_measurement_units[i].endsWith("x")) {
+				ingredient_measurement_units[i] = ingredient_measurement_units[i] + "es";
 			} else {
-				modified_ingredient_measurement_units[i] = modified_ingredient_measurement_units[i] + "s";
+				ingredient_measurement_units[i] = ingredient_measurement_units[i] + "s";
 			}
-		} else if (Number(multiplied_decimal_ingredients[i]) <= Number(1) && modified_ingredient_measurement_units[i].match(/s$/g) == "s") {
+		} else if (Number(multiplied_decimal_ingredients[i]) <= Number(1) && ingredient_measurement_units[i].match(/s$/g) == "s") {
 			//plural to singular
-			if (modified_ingredient_measurement_units[i].endsWith("es")) {
-				modified_ingredient_measurement_units[i] = modified_ingredient_measurement_units[i].slice(0, (modified_ingredient_measurement_units[i].length-2));
+			if (ingredient_measurement_units[i].endsWith("es")) {
+				ingredient_measurement_units[i] = ingredient_measurement_units[i].slice(0, (ingredient_measurement_units[i].length-2));
 			} else {
-				modified_ingredient_measurement_units[i] = modified_ingredient_measurement_units[i].slice(0, (modified_ingredient_measurement_units[i].length-1));
+				ingredient_measurement_units[i] = ingredient_measurement_units[i].slice(0, (ingredient_measurement_units[i].length-1));
 			}
 		}
 		//displays the measurement unit
-		document.getElementsByClassName("measure_unit")[i].innerHTML = modified_ingredient_measurement_units[i];
-		
-		modified_ingredient_measurement_units[i] = original_ingredient_measurement_units[i];
+		document.getElementsByClassName("measure_unit")[i].innerHTML = ingredient_measurement_units[i];				
 	}
-} 
+}
